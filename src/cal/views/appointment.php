11e8d810474d6ffab53c1dafae0a2e01229d54b5
Dave Bray <david@brayworth.com.au>
2021-03-10 16:31:37 +1000

    flexible time format

--- src/cal/views/appointment.php
+++ src/cal/views/appointment.php
@@ -10,14 +10,15 @@
 
 namespace dvc\cal;
 
-use strings;  ?>
+use strings;
+use theme;  ?>
 <form id="<?= $_form = strings::rand() ?>" autocomplete="off">
   <input type="hidden" name="action" value="appointment-save">
 
   <div class="modal fade" tabindex="-1" role="dialog" id="<?= $_modal = strings::rand() ?>" aria-labelledby="<?= $_modal ?>Label" aria-hidden="true">
     <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
       <div class="modal-content">
-        <div class="modal-header bg-secondary text-white py-2">
+        <div class="modal-header <?= theme::modalHeader() ?> py-2">
           <h5 class="modal-title" id="<?= $_modal ?>Label"><?= $this->title ?></h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
@@ -36,13 +37,13 @@ use strings;  ?>
               <div class="form-row">
                 <div class="col">
                   <div class="input-group">
-                    <input type="time" class="form-control" name="start" required>
+                    <input type="text" class="form-control" name="start" required>
 
                     <div class="input-group-append">
                       <div class="input-group-text">-</div>
                     </div>
 
-                    <input type="time" class="form-control" name="end" required>
+                    <input type="text" class="form-control" name="end" required>
 
                   </div>
 
@@ -93,6 +94,23 @@ use strings;  ?>
   </div>
   <script>
   ( _ => $(document).ready( () => {
+    $('input[name="start"]', '#<?= $_form ?>').on( 'change', function(e) {
+      CheckTimeFormat.call( this);
+
+      let s = $(this).val();
+      if ( s == '' ) return;
+
+      let j = timeHandler( s);
+      // console.log( j.toString());
+
+      j.Minutes += 30;
+
+      $('input[name="end"]', '#<?= $_form ?>').val(j.toString());
+
+    });
+
+    $('input[name="end"]', '#<?= $_form ?>').on( 'change', CheckTimeFormat);
+
     $('#<?= $_form ?>')
     .on( 'submit', function( e) {
       let _form = $(this);
