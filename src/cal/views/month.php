dd992fc4d07c61845fb7bd81f46b8b08a4e57c34
Dave Bray <david@brayworth.com.au>
2024-11-19 18:17:12 +1000

    upgrade to bs5

--- src/cal/views/month.php
+++ src/cal/views/month.php
@@ -6,23 +6,49 @@
  *
  * MIT License
  *
-*/
+ * replace:
+ * [x] data-dismiss => data-bs-dismiss
+ * [x] data-toggle => data-bs-toggle
+ * [x] data-content => data-bs-content
+ * [x] data-title => data-bs-title
+ * [x] data-trigger => data-bs-trigger
+ * [x] data-html => data-bs-html
+ * [x] data-parent => data-bs-parent
+ * [x] text-right => text-end
+ * [x] custom-select - form-select
+ * [x] mr-* => me-*
+ * [x] ml-* => ms-*
+ * [x] pr-* => pe-*
+ * [x] pl-* => ps-*
+ * [x] badge-pill rounded-pill
+ * [x] badge-primary text-bg-primary
+ * [x] badge-warning text-bg-warning
+ * [x] font-weight-bold => fw-bold
+ * [x] font-italic => fst-italic
+ * [x] input-group-prepend - remove
+ * [x] input-group-append - remove
+ * [x] btn input-group-text => btn btn-light
+ * [x] form-row => row g-2
+ * [x] form-group => mb-2
+ * [x] class="close" => class="btn-close"
+ */
 
 namespace dvc\cal;
 
 use DateTime;
 use DateInterval;
-use strings;
 
-$seed = new DateTime( $this->data->seed);
+extract((array)$this->data);
+
+$seed = new DateTime( $seed);
 $month = $seed->format('n');
 ?>
 
-<div class="form-row">
+<div class="row g-2">
   <div class="col my-2 d-flex" heading><h6 class="my-0 mx-auto p-1 text-center"><?= $seed->format('F') ?></h6></div>
 
 </div>
-<div class="form-row border-bottom">
+<div class="row g-2 border-bottom">
   <div class="col text-center small">Sun</div>
   <div class="col text-center small">Mon</div>
   <div class="col text-center small">Tue</div>
@@ -35,7 +61,7 @@ $month = $seed->format('n');
 <?php
 $day = 0;
 $hardWidth = ((int)(100 / 7 * 100))/100;
-print '<div class="form-row mb-1">';
+print '<div class="row g-2 mb-1">';
 while ( $day < $seed->format('w')) {
   $day ++;
   printf( '<div class="col" style="width: %s%%;">&nbsp;</div>', $hardWidth);
@@ -44,7 +70,7 @@ while ( $day < $seed->format('w')) {
 while ( $month == $seed->format('n')) {
 
   printf(
-    '<div class="col py-2" style="min-height: 6rem; width: %s%%;" data-date="%s"><div class="form-row"><div class="col bg-light d-flex" headline><div>%s<sup>%s</sup></div></div></div></div>',
+    '<div class="col py-2" style="min-height: 6rem; width: %s%%;" data-date="%s"><div class="row g-2"><div class="col bg-light d-flex" headline><div>%s<sup>%s</sup></div></div></div></div>',
     $hardWidth,
     $seed->format( 'Y-m-d'),
     $seed->format( 'j'),
@@ -54,7 +80,7 @@ while ( $month == $seed->format('n')) {
 
   if ( ++$day == 7) {
     $day = 0;
-    print '</div><div class="form-row mb-1">';
+    print '</div><div class="row g-2 mb-1">';
 
   }
 
--- src/cal/views/week.php
+++ src/cal/views/week.php
@@ -6,7 +6,32 @@
  *
  * MIT License
  *
-*/
+ * replace:
+ * [x] data-dismiss => data-bs-dismiss
+ * [x] data-toggle => data-bs-toggle
+ * [x] data-content => data-bs-content
+ * [x] data-title => data-bs-title
+ * [x] data-trigger => data-bs-trigger
+ * [x] data-html => data-bs-html
+ * [x] data-parent => data-bs-parent
+ * [x] text-right => text-end
+ * [x] custom-select - form-select
+ * [x] mr-* => me-*
+ * [x] ml-* => ms-*
+ * [x] pr-* => pe-*
+ * [x] pl-* => ps-*
+ * [x] badge-pill rounded-pill
+ * [x] badge-primary text-bg-primary
+ * [x] badge-warning text-bg-warning
+ * [x] font-weight-bold => fw-bold
+ * [x] font-italic => fst-italic
+ * [x] input-group-prepend - remove
+ * [x] input-group-append - remove
+ * [x] btn input-group-text => btn btn-light
+ * [x] form-row => row g-2
+ * [x] form-group => mb-2
+ * [x] class="close" => class="btn-close"
+ */
 
 namespace dvc\cal;
 
@@ -14,9 +39,11 @@ use DateTime;
 use DateInterval;
 use strings;
 
+extract((array)$this->data);
+/** @var string $seed */
 $slots = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];  ?>
 
-<div class="form-row mb-1">
+<div class="row g-2 mb-1">
   <div class="col-1">
 
     <div class="row">
@@ -25,13 +52,13 @@ $slots = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];  ?>
   </div>
 
   <?php
-  $seed = new DateTime($this->data->seed);
+  $ds = new DateTime($seed);
   for ($i = 0; $i < 7; $i++) {
-    if ($i > 0) $seed->add(new DateInterval('P1D'));  ?>
+    if ($i > 0) $ds->add(new DateInterval('P1D'));  ?>
 
-    <div class="col" data-date="<?= $seed->format('Y-m-d') ?>">
+    <div class="col" data-date="<?= $ds->format('Y-m-d') ?>">
       <div class="d-flex">
-        <h6 class="flex-fill mt-2 mb-0"><?= $seed->format('D j') ?><sup><?= $seed->format('S') ?></sup></h6>
+        <h6 class="flex-fill mt-2 mb-0"><?= $ds->format('D j') ?><sup><?= $ds->format('S') ?></sup></h6>
         <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
           <div class="btn-group btn-group-sm" role="group" aria-label="small button group"
             id="<?= $_uid = strings::rand() ?>">
@@ -47,18 +74,18 @@ $slots = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];  ?>
 
 </div>
 
-<div class="form-row border-bottom d-none" style="min-height: 4rem;" day-slot>
+<div class="row g-2 border-bottom d-none" style="min-height: 4rem;" day-slot>
 
   <div class="col-1"></div>
 
   <?php
-  $seed = new DateTime($this->data->seed);
+  $ds = new DateTime($seed);
   for ($i = 0; $i < 7; $i++) {
-    if ($i > 0) $seed->add(new DateInterval('P1D'));
+    if ($i > 0) $ds->add(new DateInterval('P1D'));
 
     printf(
       '<div class="col py-2" style="width: 13.09%%" data-date="%s" data-slot="day"></div>',
-      $seed->format('Y-m-d')
+      $ds->format('Y-m-d')
     );
   } ?>
 </div>
@@ -66,7 +93,7 @@ $slots = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];  ?>
 <?php
 foreach ($slots as $slot) { ?>
 
-  <div class="form-row border-bottom" style="min-height: 4rem;">
+  <div class="row g-2 border-bottom" style="min-height: 4rem;">
 
     <?php
     printf(
@@ -76,13 +103,13 @@ foreach ($slots as $slot) { ?>
 
     );
 
-    $seed = new DateTime($this->data->seed);
+    $ds = new DateTime($seed);
     for ($i = 0; $i < 7; $i++) {
-      if ($i > 0) $seed->add(new DateInterval('P1D'));
+      if ($i > 0) $ds->add(new DateInterval('P1D'));
 
       printf(
         '<div class="col py-2" style="width: 13.09%%" data-date="%s" data-slot="%s"></div>',
-        $seed->format('Y-m-d'),
+        $ds->format('Y-m-d'),
         $slot
       );
     } ?>
